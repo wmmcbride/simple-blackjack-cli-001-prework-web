2fa19a2fb3590481a890194b86820a57024fcb2f
wmmcbride <w.matt.mcbride@gmail.com>
2015-11-14 18:15:41 +0000

    nitrous-backup at Sat Nov 14 18:15:41 UTC 2015

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,13 +1,17 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  card = rand(1...11)
+  return card
 end
 
-def display_card_total
+def display_card_total(card)
   # code #display_card_total here
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
@@ -24,6 +28,11 @@ end
 
 def initial_round
   # code #initial_round here
+  hand = 0
+  2.times do
+    hand = hand + deal_card()
+  end
+  display_card_total(hand)
 end
 
 def hit?
@@ -41,4 +50,4 @@ end
 def runner
   # code runner here
 end
-    
+
